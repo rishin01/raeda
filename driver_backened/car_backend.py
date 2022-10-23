3bafe060d4b4b4660abdbec6fda63b394fd3e4c6
Caitlin <csfrank05@gmail.com>
2022-10-23 07:36:51 +0100

    Playing with car backend

--- driver_backened/car_backend.py
+++ driver_backened/car_backend.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from brookie.scripts.blockchain_interaction import *
+from taxidata import TaxiData
+import multiprocessing as mp
+
+
+taxi_datas = [TaxiData for i in range(mp.cpu_count())]
 
 # check for broadcast of pair that includes your own address
-my_address = 'my_wallet_address'
-
 def is_pair():
     '''
     reads pair from the blockchain, checks whether the pair includes your own address.
     '''
-    pair = get_pair_info()
-    if pair.loc['Address']==my_address:
+	search_and_find_pair(my_address)
+    # if pair.loc['Address']==my_address:
         # send to blockchain that payment is made
         # edit taxidata so that available = 0
-    else:
-        # ?
+
 
 # check your own wallet to see if payment has been made
 
--- taxidata.py
+++ taxidata.py
@@ -1,7 +1,8 @@
+from blondie.token.scripts.blockchain_interaction
 
-class TaxiData: 
-	def __init__():
-		self.taxi_address = ''
+class TaxiData:
+	def __init__(self):
+		#randomise starting values here
 		self.id = -1
 		self.x = -1
 		self.y = -1
@@ -9,40 +10,42 @@ class TaxiData:
 		self.base_price = -1
 		self.available = -1
 		self._isAlive = False
-	
-	def setints(intsarray):
-		self.id = intsarray[0]
-		self.x = intsarray[1]
-		self.y = intsarray[2]
-		self.price_min = intsarray[3]
-		self.base_price = intsarray[4]
-		
-	def setaddresses(address):
-		self.address = address
-	
-	def setbools(boolsarray):
-		if boolsarray[0]:
-			self.available = 1
-		else:
-			self.available = 0
-		self._isAlive = boolsarray[1]
-	
-	def returnarray():
-		return [self.taxi_address,self.x,self.y,self.base_price,self.price_min,self.available]
 
-alltaxiints = get_from_blockchain_ints()
-alltaxiaddresses = get_from_blockchain_addresses()
-alltaxibools = get_from_blockchain_bools()
-taxidatas = [TaxiData() for i in range(len(alltaxiints))]
-i = 0
-for td in taxidatas:
-	td.setints(alltaxiints[i])
-	td.setaddresses(alltaxiaddresses[i])
-	td.setbools(alltaxibools[i])
-	i+=1
+	def upload(self):
 
-def listofalltaxidata():
-	l = []
-	for td in taxidatas:
-		l.append(td.returnarray())
-	return l
+	# def setints(self,intsarray):
+	# 	self.id = intsarray[0]
+	# 	self.x = intsarray[1]
+	# 	self.y = intsarray[2]
+	# 	self.price_min = intsarray[3]
+	# 	self.base_price = intsarray[4]
+
+	# def setaddresses(address):
+	# 	self.address = address
+	#
+	# def setbools(boolsarray):
+	# 	if boolsarray[0]:
+	# 		self.available = 1
+	# 	else:
+	# 		self.available = 0
+	# 	self._isAlive = boolsarray[1]
+
+	# def returnarray():
+	# 	return [self.taxi_address,self.x,self.y,self.base_price,self.price_min,self.available]
+
+# alltaxiints = get_from_blockchain_ints()
+# alltaxiaddresses = get_from_blockchain_addresses()
+# alltaxibools = get_from_blockchain_bools()
+# taxidatas = [TaxiData() for i in range(len(alltaxiints))]
+# i = 0
+# for td in taxidatas:
+# 	td.setints(alltaxiints[i])
+# 	td.setaddresses(alltaxiaddresses[i])
+# 	td.setbools(alltaxibools[i])
+# 	i+=1
+#
+# def listofalltaxidata():
+# 	l = []
+# 	for td in taxidatas:
+# 		l.append(td.returnarray())
+# 	return l
