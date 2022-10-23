cebed1e9fcc055592b27c3c3bb1818658c2fba47
Rishin Madan <rishin.madan@gmail.com>
2022-10-23 06:57:34 +0100

    added to taxidata

--- driver_backened/car_backend.py
+++ driver_backened/car_backend.py
@@ -1,22 +1,19 @@
 import pandas as pd
 from brookie.scripts.blockchain_interaction import *
 
-# This will eventually be the taxi front end pressing a button to say available
-# need to randomly set starting location, and will receive pricing info, and available should always be made 1
-taxi_info = pd.Series(data=[1,2,3,4,1],
-                      index=['X location', 'Y location', 'Flat Rate', 'Price/min', 'Available'],
-                      name='car4'
-                     )
-
-def set_availability(taxi_info):
-    '''
-    add taxi availabile to blockchain dataset (including updates of price etc)
-    '''
-    add_taxi(taxi_info)
-    return
-
 # check for broadcast of pair that includes your own address
-def
+my_address = 'my_wallet_address'
+
+def is_pair():
+    '''
+    reads pair from the blockchain, checks whether the pair includes your own address.
+    '''
+    pair = get_pair_info()
+    if pair.loc['Address']==my_address:
+        # send to blockchain that payment is made
+        # edit taxidata so that available = 0
+    else:
+        # ?
 
 # check your own wallet to see if payment has been made
 
